05952ff76b3a1b13d095ff7df45f83425376ece0
Mohamad Hesari <mdhesari99@gmail.com>
2022-07-23 11:30:50 +0430

    has views trait

---
+++ src/Traits/HasViews.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mdhesari\LaravelViews\Traits;
+
+use Mdhesari\LaravelViews\Models\View;
+
+trait HasViews
+{
+    /**
+     * Define a polymorphic one-to-many relationship.
+     *
+     * @param string $related
+     * @param string $name
+     * @param string|null $type
+     * @param string|null $id
+     * @param string|null $localKey
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    abstract public function morphMany($related, $name, $type = null, $id = null, $localKey = null): \Illuminate\Database\Eloquent\Relations\MorphMany;
+
+    public function views(): \Illuminate\Database\Eloquent\Relations\MorphMany
+    {
+        return $this->morphMany(View::class, 'viewable');
+    }
+}
